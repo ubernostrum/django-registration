97f4249bd8f3ef6cd0ae1bc068e236e8575143ee
James Bennett <james@b-list.org>
2015-09-13 03:07:03 -0500

    Actually I like it better this way.

--- registration/tests/urls.py
+++ registration/tests/urls.py
@@ -18,37 +18,31 @@ urlpatterns = [
         TemplateView.as_view(
             template_name='registration/activation_complete.html'
         ),
-        name='simple_activation_redirect'
-    ),
+        name='simple_activation_redirect'),
     url(r'^activate/complete/$',
         TemplateView.as_view(
             template_name='registration/activation_complete.html'
         ),
-        name='registration_activation_complete'
-    ),
+        name='registration_activation_complete'),
     # Activation keys get matched by \w+ instead of the more specific
     # [a-fA-F0-9]{40} because a bad activation key should still get to
     # the view; that way it can return a sensible "invalid key"
     # message instead of a confusing 404.
     url(r'^activate/(?P<activation_key>\w+)/$',
         ActivateWithSimpleRedirect.as_view(),
-        name='registration_activate'
-    ),
+        name='registration_activate'),
     url(r'^register/$',
         RegistrationView.as_view(),
-        name='registration_register'
-    ),
+        name='registration_register'),
     url(r'^register/complete/$',
         TemplateView.as_view(
             template_name='registration/registration_complete.html'
         ),
-        name='registration_complete'
-    ),
+        name='registration_complete'),
     url(r'^register/closed/$',
         TemplateView.as_view(
             template_name='registration/registration_closed.html'
         ),
-        name='registration_disallowed'
-    ),
+        name='registration_disallowed'),
     url(r'', include('registration.auth_urls')),
 ]
