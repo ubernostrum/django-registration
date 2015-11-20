a2b08cdfa947e7da008402f0e8bbd01a520ac4c1
James Bennett <james@b-list.org>
2015-11-20 01:30:49 -0600

    Docstring cleanup in models.py.
    Most of this is eliminating excessive details in docstrings; full
    documentation exists in the documentation, and the docstrings are now
    shorter and simply provide general descriptions.

--- registration/models.py
+++ registration/models.py
@@ -1,7 +1,7 @@
 """
-Model and manager used by the two-step (sign up, then activate)
-workflow. If you're not using that workflow, you don't need to have
-'registration' in your INSTALLED_APPS.
+Model and manager used by the model-based activation workflow. If
+you're not using that workflow, you don't need to have 'registration'
+in your INSTALLED_APPS.
 
 This is provided primarily for backwards-compatibility with existing
 installations; new installs of django-registration should look into
@@ -39,21 +39,9 @@ class RegistrationManager(models.Manager):
     """
     def activate_user(self, activation_key):
         """
-        Validate an activation key and activate the corresponding
-        user if valid.
-
-        If the key is valid and has not expired, return the user after
-        activating.
-
-        If the key is not valid or has expired, return ``False``.
-
-        If the key is valid but the user is already active, return
-        ``False``.
-
-        To prevent reactivation of an account which has been
-        deactivated by site administrators, the activation key is
-        reset to the string constant ``RegistrationProfile.ACTIVATED``
-        after successful activation.
+        Validate an activation key and activate the corresponding user
+        if valid. Returns the user account on success, ``False`` on
+        failure.
 
         """
         # Make sure the key we're trying conforms to the pattern of a
@@ -79,9 +67,6 @@ class RegistrationManager(models.Manager):
         ``RegistrationProfile`` and email its activation key to the
         user, returning the new user.
 
-        By default, an activation email will be sent to the new
-        user. To disable this, pass ``send_email=False``.
-
         """
         new_user = form.save(commit=False)
         new_user.is_active = False
@@ -100,10 +85,6 @@ class RegistrationManager(models.Manager):
         Create a ``RegistrationProfile`` for a given user, and return
         the ``RegistrationProfile``.
 
-        The activation key for the ``RegistrationProfile`` will be a
-        SHA1 hash, generated from a combination of the user's username
-        and a random salt.
-
         """
         User = get_user_model()
         username = str(getattr(user, User.USERNAME_FIELD))
@@ -118,40 +99,6 @@ class RegistrationManager(models.Manager):
         Remove expired instances of ``RegistrationProfile`` and their
         associated users.
 
-        Accounts to be deleted are identified by searching for
-        instances of ``RegistrationProfile`` with expired activation
-        keys, and then checking to see if their associated user
-        instances have the field ``is_active`` set to ``False``; any
-        user who is both inactive and has an expired activation key
-        will be deleted.
-
-        It is recommended that this method be executed regularly as
-        part of your routine site maintenance; this application
-        provides a custom management command which will call this
-        method, accessible as ``manage.py cleanupregistration``.
-
-        Regularly clearing out accounts which have never been
-        activated serves two useful purposes:
-
-        1. It alleviates the ocasional need to reset a
-           ``RegistrationProfile`` and/or re-send an activation email
-           when a user does not receive or does not act upon the
-           initial activation email; since the account will be
-           deleted, the user will be able to simply re-register and
-           receive a new activation key.
-
-        2. It prevents the possibility of a malicious user registering
-           one or more accounts and never activating them (thus
-           denying the use of those usernames to anyone else); since
-           those accounts will be deleted, the usernames will become
-           available for use again.
-
-        If you have a troublesome user and wish to disable their
-        account while keeping it in the database, simply delete the
-        associated ``RegistrationProfile``; an inactive user which
-        does not have an associated ``RegistrationProfile`` will not
-        be deleted.
-
         """
         for profile in self.all():
             if profile.activation_key_expired():
@@ -164,18 +111,8 @@ class RegistrationManager(models.Manager):
 @python_2_unicode_compatible
 class RegistrationProfile(models.Model):
     """
-    A simple profile which stores an activation key for use during
-    user account registration.
-
-    Generally, you will not want to interact directly with instances
-    of this model; the provided manager includes methods for creating
-    and activating new accounts, as well as for cleaning out accounts
-    which have never been activated.
-
-    While it is possible to use this model as the value of the
-    ``AUTH_PROFILE_MODULE`` setting, it's not recommended that you do
-    so. This model's sole purpose is to store data temporarily during
-    account registration and activation.
+    A simple model which stores an activation key for use during user
+    account registration.
 
     """
     ACTIVATED = u"ALREADY_ACTIVATED"
@@ -197,22 +134,7 @@ class RegistrationProfile(models.Model):
         """
         Determine whether this ``RegistrationProfile``'s activation
         key has expired, returning a boolean -- ``True`` if the key
-        has expired.
-
-        Key expiration is determined by a two-step process:
-
-        1. If the user has already activated, the key will have been
-           reset to the string constant ``ACTIVATED``. Re-activating
-           is not permitted, and so this method returns ``True`` in
-           this case.
-
-        2. Otherwise, the date the user signed up is incremented by
-           the number of days specified in the setting
-           ``ACCOUNT_ACTIVATION_DAYS`` (which should be the number of
-           days after signup during which a user is allowed to
-           activate their account); if the result is less than or
-           equal to the current date, the key has expired and this
-           method returns ``True``.
+        has expired, ``False`` otherwise.
 
         """
         expiration_date = datetime.timedelta(
@@ -226,38 +148,6 @@ class RegistrationProfile(models.Model):
         """
         Send an activation email to the user associated with this
         ``RegistrationProfile``.
-
-        The activation email will make use of two templates:
-
-        ``registration/activation_email_subject.txt``
-            This template will be used for the subject line of the
-            email. Because it is used as the subject line of an email,
-            this template's output **must** be only a single line of
-            text; output longer than one line will be forcibly joined
-            into only a single line.
-
-        ``registration/activation_email.txt``
-            This template will be used for the body of the email.
-
-        These templates will each receive the following context
-        variables:
-
-        ``activation_key``
-            The activation key for the new account.
-
-        ``expiration_days``
-            The number of days remaining during which the account may
-            be activated.
-
-        ``site``
-            An object representing the site on which the user
-            registered; depending on whether ``django.contrib.sites``
-            is installed, this may be an instance of either
-            ``django.contrib.sites.requests.Site`` (if the sites
-            application is installed) or
-            ``django.contrib.sites.models.RequestSite`` (if
-            not). Consult the documentation for the Django sites
-            framework for details regarding these objects' interfaces.
 
         """
         ctx_dict = {'activation_key': self.activation_key,
