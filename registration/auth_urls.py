1a0068df703a646f2ccefcdce89502162d15efe2
James Bennett <james@b-list.org>
2017-06-14 22:54:07 -0700

    Fix #94: Use class-based auth views when available.

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -12,7 +12,7 @@ necessary to manually include these views.
 import textwrap
 import warnings
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 
 
@@ -26,36 +26,11 @@ warnings.warn(
 )
 
 
+auth_url_module = 'registration.auth_urls_classes' if \
+                  hasattr(auth_views, 'LoginView') else \
+                  'registration.auth_urls_functions'
+
+
 urlpatterns = [
-    url(r'^login/$',
-        auth_views.login,
-        {'template_name': 'registration/login.html'},
-        name='auth_login'),
-    url(r'^logout/$',
-        auth_views.logout,
-        {'template_name': 'registration/logout.html'},
-        name='auth_logout'),
-    url(r'^password/change/$',
-        auth_views.password_change,
-        {'post_change_redirect': 'auth_password_change_done'},
-        name='auth_password_change'),
-    url(r'^password/change/done/$',
-        auth_views.password_change_done,
-        name='auth_password_change_done'),
-    url(r'^password/reset/$',
-        auth_views.password_reset,
-        {'post_reset_redirect': 'auth_password_reset_done',
-         'email_template_name': 'registration/password_reset_email.txt'},
-        name='auth_password_reset'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/'
-        r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,
-        {'post_reset_redirect': 'auth_password_reset_complete'},
-        name='auth_password_reset_confirm'),
-    url(r'^password/reset/complete/$',
-        auth_views.password_reset_complete,
-        name='auth_password_reset_complete'),
-    url(r'^password/reset/done/$',
-        auth_views.password_reset_done,
-        name='auth_password_reset_done'),
+    url(r'', include(auth_url_module)),
 ]
---
+++ registration/auth_urls_classes.py
@@ -0,0 +1,42 @@
+from django.conf.urls import url
+
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    url(r'^login/$',
+        auth_views.LoginView.as_view(
+            template_name='registration/login.html'
+        ),
+        name='auth_login'),
+    url(r'^logout/$',
+        auth_views.LogoutView.as_view(
+            template_name='registration/logout.html'
+        ),
+        name='auth_logout'),
+    url(r'^password/change/$',
+        auth_views.PasswordChangeView.as_view(
+            success_url='auth_password_change_done'
+        ),
+        name='auth_password_change'),
+    url(r'^password/change/done/$',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='auth_password_change_done'),
+    url(r'^password/reset/$',
+        auth_views.PasswordResetView.as_view(
+            success_url='auth_password_reset_done'
+        ),
+        name='auth_password_reset'),
+    url(r'^password/reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='auth_password_reset_complete'),
+    url(r'^password/reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='auth_password_reset_done'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/'
+        r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            success_url='auth_password_reset_complete'
+        ),
+        name='auth_password_reset_confirm'),
+]
