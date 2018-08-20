4298e82a3dc4c6577b41b4acbb73ff7bb5795002
James Bennett <james@b-list.org>
2018-08-20 04:04:05 -0700

    Make the one-step backend a little more robust with custom users.

--- src/django_registration/backends/one_step/views.py
+++ src/django_registration/backends/one_step/views.py
@@ -25,10 +25,10 @@ class RegistrationView(BaseRegistrationView):
 
     def register(self, form):
         new_user = form.save()
-        new_user = authenticate(
-            username=getattr(new_user, User.USERNAME_FIELD),
-            password=form.cleaned_data['password1']
-        )
+        new_user = authenticate(**{
+            User.USERNAME_FIELD: getattr(new_user, User.USERNAME_FIELD),
+            'password': form.cleaned_data['password1']
+        })
         login(self.request, new_user)
         signals.user_registered.send(
             sender=self.__class__,
