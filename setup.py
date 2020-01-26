289e1e266cead530b356012097e4d6f1aaf958b8
James Bennett <james@b-list.org>
2020-01-26 15:13:53 -0800

    Bump version numbers.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name="django-registration",
     zip_safe=False,  # eggs are the devil.
-    version="3.1a1",
+    version="3.1",
     description="An extensible user-registration application for Django",
     long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
     author="James Bennett",
