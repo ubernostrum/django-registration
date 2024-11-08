d217015ddbfd0b679b6eb3eccec542ce0fd8ad8a
James Bennett <james@b-list.org>
2024-11-07 22:18:00 -0800

    Revert since now RTD is picking up the 5.1.0 tag.

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ https://www.sphinx-doc.org/
 
 import os
 import sys
+from importlib.metadata import version as get_version
 
 import django
 from django.conf import settings
@@ -38,8 +39,8 @@ source_suffix = {".rst": "restructuredtext"}
 master_doc = "index"
 project = "django-registration"
 copyright = "James Bennett and contributors"
-version = "5.1.0"
-release = "5.1.0"
+version = get_version("django-registration")
+release = version
 exclude_trees = ["_build"]
 pygments_style = "sphinx"
 htmlhelp_basename = "django-registrationdoc"
